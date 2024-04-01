432b436b1f1c10d15c3c7d4c6989466022ab8ede
MuhammadBilalAkbar <mbilalakbarkh786@gmail.com>
2024-04-01 10:30:02 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Barcode Scan',
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          appBarTheme: const AppBarTheme(
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              fontSize: 50.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          textTheme: const TextTheme(
+            bodyMedium: TextStyle(fontSize: 30),
+            labelLarge: TextStyle(fontSize: 30),
+          ),
+        ),
+        home: const BarCodeScanner(),
+      );
+}
+
+class BarCodeScanner extends StatefulWidget {
+  const BarCodeScanner({super.key});
+
+  @override
+  BarCodeScannerState createState() => BarCodeScannerState();
+}
+
+class BarCodeScannerState extends State<BarCodeScanner> {
+  String scanBarcodeResult = 'Unknown';
+
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'Cancel',
+        true,
+        ScanMode.BARCODE,
+      );
+      debugPrint(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      scanBarcodeResult = barcodeScanRes;
+    });
+  }
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'Cancel',
+        true,
+        ScanMode.QR,
+      );
+      debugPrint(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+    if (!mounted) return;
+
+    setState(() {
+      scanBarcodeResult = barcodeScanRes;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Barcode Scan'),
+        ),
+        body: Builder(
+          builder: (context) => Container(
+            alignment: Alignment.center,
+            child: Flex(
+              direction: Axis.vertical,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: scanBarcodeNormal,
+                  child: const Text('Start barcode scan'),
+                ),
+                ElevatedButton(
+                  onPressed: scanQR,
+                  child: const Text('Start QR scan'),
+                ),
+                Text('Scan result : $scanBarcodeResult\n'),
+              ],
+            ),
+          ),
+        ),
+      );
+}
